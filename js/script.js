cd9875bede4e495f6e4734b3fee3d7f31f34aa49
akari <brownie69723@gmail.com>
2021-01-21 15:52:52 +0900

    Github入門

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(function(){
+    $(".warp1").on('click', function(){
+        $("html,body").animate({scrollTop:$(".q1").offset().top}, 700);
+    });
+    $(".warp2").on('click', function(){
+        $("html,body").animate({scrollTop:$(".q2").offset().top}, 700);
+    });
+    $(".warp3").on('click', function(){
+        $("html,body").animate({scrollTop:$(".q4").offset().top}, 1000);
+    });
+    $(".warp4").on('click', function(){
+        $("html,body").animate({scrollTop:$(".a1").offset().top}, 1000);
+    });
+    $(".warp5").on('click', function(){
+        $("html,body").animate({scrollTop:$(".q3").offset().top}, 700);
+    });
+    $(".warp6").on('click', function(){
+        $("html,body").animate({scrollTop:$(".a1").offset().top}, 1000);
+    });
+    $(".warp7").on('click', function(){
+        $("html,body").animate({scrollTop:$(".a2").offset().top}, 1000);
+    });
+    $(".warp8").on('click', function(){
+        $("html,body").animate({scrollTop:$(".q5").offset().top}, 700);
+    });
+    $(".warp9").on('click', function(){
+        $("html,body").animate({scrollTop:$(".a2").offset().top}, 1000);
+    });
+    $(".warp10").on('click', function(){
+        $("html,body").animate({scrollTop:$(".a3").offset().top}, 1000);
+    });
+    $(".warp11").on('click', function(){
+        $("html,body").animate({scrollTop:$(".a4").offset().top}, 1000);
+    });
+    $("#top-btn").click(function (){
+        $("html,body").animate({
+            scrollTop: 0
+        }, 700);
+    });
+});
+
+
